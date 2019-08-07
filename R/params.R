e754e4067d023c071d90901550c1fbeb66dacceb
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-06 22:33:35 -0400

    Add params

--- R/params.R
+++ R/params.R
@@ -19,6 +19,8 @@
 #' @param asis `logical(1)`.
 #'   Set this to `TRUE` when using the function inside a loop or inside an R
 #'   Markdown chunk with '`results = "asis"`' enabled.
+#' @param aspectRatio `integer(1)`.
+#'   Aspect ratio.
 #' @param assay `vector(1)`.
 #'   Assay name or index position.
 #' @param assays `SimpleList`.
@@ -26,6 +28,8 @@
 #'   in as either a dense matrix (`matrix`) or sparse matrix (`sparseMatrix`).
 #' @param base `integer(1)`.
 #'   Logarithm base.
+#' @param borderColor `character(1)` or `NULL`.
+#'   Border color.
 #' @param captions `character` or `NULL`.
 #'   Captions.
 #'   If `NULL`, the names of the primary input list will be used automatically.
@@ -45,13 +49,34 @@
 #'   cells.
 #' @param colnames `logical(1)`.
 #'   Apply to column names.
+#' @param color `ScaleDiscrete`.
+#'   Desired ggplot2 color scale. Must supply discrete values. When set `NULL`,
+#'   the default ggplot2 color palette will be used. If manual color definitions
+#'   are desired, we recommend using [ggplot2::scale_color_manual()].
+#'
+#'   To set the discrete color palette globally, use:
+#'
+#'   ```
+#'   options(acid.color.discrete = ggplot2::scale_color_viridis_d())
+#'   ```
 #' @param compress `logical(1)` or `character(1)`.
 #'   These character strings are currently allowed for [`save()`][base::save]:
 #'   `"gzip"`, `"bzip2"`, or `"xz"`.
 #' @param counts `matrix`.
 #'   Count matrix. Normalized counts are recommended.
+#' @param countsAxisLabel `character(1)`.
+#'   Counts axis label.
+#' @param dark `logical(1)`.
+#'   Plot against a dark background using the
+#'   [`acid_theme_light()`][acidplots::acid_theme_light] ggplot2 theme.
+#' @param dims `integer`.
+#'   Vector of length 2 that denotes the columns from the reduced dimension
+#'   matrix to use for `centerX` and `centerY` column calculations. Defaults the
+#'   first and second dimensions.
 #' @param dir `character(1)`.
 #'   Directory path.
+#' @param direction `character(1)`.
+#'   Plot "`both`", "`up`", or "`down`" directions.
 #' @param ensemblRelease,release `integer(1)`.
 #'   Ensembl release version (e.g. `90`). We recommend setting this value if
 #'   possible, for improved reproducibility. When left unset, the latest release
@@ -64,24 +89,37 @@
 #'   Calculation to apply.
 #'   Uses [`match.arg()`][base::match.arg] internally and defaults to the first
 #'   argument in the `character` vector.
+#' @param FUN `function`.
 #' @param file `character(1)`.
 #'   File path.
-#' @param FUN `function`.
+#' @param fill `ggproto`/`ScaleDiscrete`.
+#'   Desired ggplot2 fill scale. Must supply discrete values. When set to
+#'   `NULL`, the default ggplot2 color palette will be used. If manual color
+#'   definitions are desired, we recommend using [ggplot2::scale_fill_manual()].
+#'
+#'   To set the discrete fill palette globally, use:
+#'
+#'   ```
+#'   options(acid.fill.discrete = ggplot2::scale_fill_viridis_d())
+#'   ```
+#' @param flip `logical(1)`.
+#'   Flip x and y axes. Recommended for plots containing many samples.
 #' @param fun `function`.
 #' @param gene2symbol `Gene2Symbol`.
 #'   Gene-to-symbol mappings. Must contain `geneID` and `geneName` columns. See
 #'   `Gene2Symbol` for more information.
 #' @param genes `character`.
-#'   Gene identifiers. It is considered better practice to input the stable gene
-#'   identifiers from Ensembl (e.g. "ENSG00000000003") and not the (HGNC) gene
-#'   symbols (e.g. "TSPN6"), if possible.
+#'   Gene identifiers.
 #' @param genomeBuild `character(1)`.
 #'   Ensembl genome build assembly name (e.g. `"GRCh38"`). If set `NULL`,
 #'   defaults to the most recent build available.
 #'   Note: don't pass in UCSC build IDs (e.g. `"hg38"`).
 #' @param gffFile `character(1)`.
-#'   GFF/GTF (General Feature Format) file. Generally, we recommend using a GTF
-#'   (GFFv2) instead of a GFFv3 file if possible.
+#'   GFF/GTF (General Feature Format) file.
+#'   Generally, we recommend using GTF (GFFv2) instead of GFFv3.
+#' @param geom `character(1)`.
+#'   Plot type. Uses [`match.arg()`][base::match.arg] internally and defaults to
+#'   the first argument in the `character` vector.
 #' @param headerLevel `integer(1)` (`1`-`7`).
 #'   Markdown header level.
 #' @param i
@@ -96,22 +134,38 @@
 #' @param inherits `logical(1)`.
 #'   Should the enclosing frames of the `environment` be searched?
 #' @param interestingGroups `character`.
-#'   Groups of interest that define the samples. If left unset, defaults to
-#'   `sampleName`.
+#'   Groups of interest to use for visualization.
+#'   Corresponds to factors describing the columns of the object.
+#' @param label `logical(1)`.
+#'   Superimpose sample text labels on the plot.
+#' @param labelSize `integer(1)`.
+#'   Size of the text label.
+#' @param labs `list`.
+#'   ggplot2 labels. See [ggplot2::labs()] for details.
 #' @param level `character(1)`.
 #'   Return as genes or transcripts.
+#' @param legend `logical(1)`.
+#'   Include plot legend.
+#' @param limit `numeric(1)`.
+#'   Threshold limit.
 #' @param lower `numeric(1)`.
 #'   Lower boundary.
+#' @param max `numeric(1)`.
+#'   Recommended maximum value cutoff.
 #' @param metadata `list`.
 #'   Metadata.
 #' @param metric `character(1)`.
 #'   Metric type, either `length` or `elements`.
+#' @param min `numeric(1)`.
+#'   Recommended minimum value cutoff.
+#' @param minCounts `integer(1)`.
+#'   Minimum number of counts per feature (i.e. gene).
 #' @param msg `NULL` or `character(1)`.
 #'   Custom message to return.
 #' @param n `integer(1)`.
 #'   Number to include.
 #' @param ntop `integer(1)`.
-#'   Number of top genes to label.
+#'   Number of top features to label.
 #' @param nullOK `logical(1)`.
 #'   If set to `TRUE`, `x` may also be `NULL`.
 #' @param ordered `logical(1)`.
@@ -121,6 +175,21 @@
 #'   Overwrite existing file on disk.
 #' @param pattern `character(1)`.
 #'   Pattern to use for matching.
+#' @param perMillion `logical(1)`.
+#'   Display as counts per million.
+#' @param perSample `logical(1)`.
+#'   Visualize the distributions per sample.
+#' @param pointAlpha `numeric(1)` (`0`-`1`).
+#'   Alpha transparency level. Useful when there many points in the dataset
+#'   (e.g. single-cell data), and some points can be masked.
+#' @param pointColor `character(1)`.
+#'   Default point color for the plot.
+#' @param pointScalar `integer(1)`.
+#'   Default point size for the plot.
+#' @param pointsAsNumbers `logical(1)`.
+#'   Plot the points as numbers (`TRUE`) or dots (`FALSE`).
+#' @param pointSize `numeric(1)`.
+#'   Point size for dots in the plot.
 #' @param prefilter `logical(1)`.
 #'   Apply prefiltering. Remove zero count genes.
 #' @param progress `logical(1)`.
@@ -154,6 +223,8 @@
 #'   Resort using `sort`.
 #' @param spikeNames `character`.
 #'   Vector indicating which assay rows denote spike-in sequences (e.g. ERCCs).
+#' @param subtitle `character(1)`.
+#'   Subtitle.
 #' @param tabset `logical(1)`.
 #'   Include tabset marker.
 #' @param text `character(1)`.
@@ -164,8 +235,18 @@
 #'   Include traceback in error message.
 #'   See [`traceback()`][base::traceback] for details.
 #'  `rlang::entrace()` also works nicely and can be set in `.Rprofile`.
+#' @param trans `character(1)`.
+#'   Name of the axis scale transformation to apply.
+#'
+#'   For more information:
+#'
+#'   ```
+#'   help(topic = "scale_x_continuous", package = "ggplot2")
+#'   ```
 #' @param transgeneNames `character`.
 #'   Vector indicating which assay rows denote transgenes (e.g. EGFP, TDTOMATO).
+#' @param trendline `logical(1)`.
+#'   Include trendline on plot.
 #' @param tx2gene `Tx2Gene`.
 #'   Transcript-to-gene mappings.
 #' @param unicode `logical(1)`.
